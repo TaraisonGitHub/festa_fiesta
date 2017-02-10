109b937e07cb672cab345008c730a6b643340dbd
Tara Baker <Tara Baker>
2017-02-09 21:30:34 -0500

    created html route file and api route files for tables

---
+++ app/routes/games-api-routes.js
@@ -0,0 +1,42 @@
+var dbThree = require("../models");
+
+
+// Skeleton of route assumes that the "games" model is linked to the party database
+module.exports = function(app) {
+  app.get("/api/games", function(req, res) {
+    // get all the decorations
+    dbThree.decorations.findAll({}).then(function(results) {
+      res.json(results);
+    });
+  });
+
+
+/*
+  app.get("/api/authors/:id", function(req, res) {
+    // 2; Add a join to include all of the Author's Posts here
+    db.Author.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbAuthor) {
+      res.json(dbAuthor);
+    });
+  });
+
+  app.post("/api/authors", function(req, res) {
+    db.Author.create(req.body).then(function(dbAuthor) {
+      res.json(dbAuthor);
+    });
+  });
+
+  app.delete("/api/authors/:id", function(req, res) {
+    db.Author.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbAuthor) {
+      res.json(dbAuthor);
+    });
+  });
+*/
+};
