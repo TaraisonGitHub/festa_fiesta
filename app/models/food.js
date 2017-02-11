430d4a9e6850cb52aae02db39adde61415e2bf2b
David Agbenyega <qafui@outlook.com>
2017-02-11 11:11:26 -0500

    adding connect-ensure-login

--- app/models/food.js
+++ app/models/food.js
@@ -8,22 +8,8 @@ module.exports = function(sequelize, DataTypes) {
       }
     },
     cost: {
-<<<<<<< HEAD
       type: DataTypes.INTEGER
     }
-  }
-    // {
-//       // We're saying that we want our Guest to bring Food
-//       classMethods: {
-//         associate: function(models) {
-//           // A Guest (foreignKey) is required or Food can't be made
-//           Food.belongsTo(models.Guest, {
-//             foreignKey: {
-//               allowNull: false
-//             }
-//           });
-//         }
-//       }
-//     });
+});
   return Food;
 };
