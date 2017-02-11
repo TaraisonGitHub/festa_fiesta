34137c9c83f4321961cbe7908370c74ef28b16bc
David Agbenyega <qafui@outlook.com>
2017-02-11 10:07:56 -0500

    Setup passport-facebook

--- app/routes/login-routes.js
+++ app/routes/login-routes.js
@@ -1,9 +1,9 @@
 
-/ ===============================================================================
+// ===============================================================================
 // LOAD DATA
 // Linking our routes to our data sources.
 // ===============================================================================
-
+var passport = require('passport');
 
 
 
@@ -13,25 +13,32 @@
 
 module.exports = function(app) {
 
-    const client_id = '1144869418943929';
-    const client_secret = 'c48b01d62147cba4d2d8b2f924eb0dab';
+	// API GET Requests
+	// ---------------------------------------------------------------------------
 
-    const authorization_base_url = 'https://www.facebook.com/dialog/oauth';
-    const token_url = 'https://www.facebook.com/dialog/oauth';
-    const redirect_url = 'https://festafiesta.herokuapp.com';
+	// Define routes.
+	app.get('/',
+		function(req, res) {
+	  		res.render('home', { user: req.user });
+	});
 
-  // API GET Requests
-  // ---------------------------------------------------------------------------
+	app.get('/login',
+		function(req, res){
+	  	  	res.render('login');
+	});
 
-    //User is redirected to facebook if he/she is not yet logged in
-    app.get("/", function(req, res) {
-        if(!req.session.oauth_access_token) {
-		          res.redirect("/facebook_login");
-	     }
-    });
+	app.get('/login/facebook',
+		passport.authenticate('facebook'));
 
-    
+		app.get('/login/facebook/return',
+		passport.authenticate('facebook', { failureRedirect: '/login' }),
+		function(req, res) {
+		  res.redirect('/');
+	});
 
-
+	app.get('/profile',require('connect-ensure-login').ensureLoggedIn(),
+		function(req, res){
+	  		res.render('profile', { user: req.user });
+	});
 
 };
--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var oauth = require('oauth').OAuth;
-var querystring = require('querystring');
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+
+passport.use(new Strategy({
+	clientID: '1144869418943929',
+	clientSecret: 'CGRYW6NXCFHP2ZBUUVTNMAKT5N25652QETXLPP5FFHRKQL3YED',
+	callbackURL: 'http://https://festafiesta.herokuapp.com/login/return'
+	},
+	function(accessToken, refreshToken, profile, cb) {
+		return cb(null, profile);
+	}
+));
+
+passport.serializeUser(function(user, cb) {
+	cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+	cb(null, obj);
+});
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./app/models");
-
-app.use(express.cookieParser());
-app.use(express.session({
-    secret: 'slkdfjnlsrubgstrlgsunrtgstrq'
-}));
-
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "./app/public"));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
 // override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 var exphbs = require("express-handlebars");
@@ -31,13 +37,28 @@ app.engine("handlebars", exphbs({
 }));
 app.set("view engine", "handlebars");
 
+// Requiring our models for syncing
+var db = require("./app/models");
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
 var routes = require("./app/controllers/fiesta_controller");
 
 require("./app/routes/login-routes.js")(app);
 require("./app/routes/html-routes.js");
 require("./app/routes/api-routes.js");
 
-// listen on port 3000
+// listen on port 8080
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
